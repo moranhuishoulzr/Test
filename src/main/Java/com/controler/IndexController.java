16f0031c18100c26cc442f7861d0db7ff9fb2727
lzr <991192715@qq.com>
2019-01-02 16:42:12 +0800

    新建文件和工程

---
+++ src/main/Java/com/controler/IndexController.java
@@ -0,0 +1,19 @@
+package com.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: comit
+ * @description:
+ * @author: liangzr
+ * @create: 2019-01-02 16:30
+ */
+@Controller
+@RequestMapping("/home")
+public class IndexController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
