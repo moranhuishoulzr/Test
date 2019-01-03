bb0f9a85fcb813c01880eadc5abcd3241d136147
lzr <991192715@qq.com>
2019-01-03 11:20:16 +0800

    问题改正

--- src/main/Java/com/controler/IndexController.java
+++ src/main/Java/com/controler/IndexController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/index")
     public String index() {
-        return 0;
         return "index";
     }
 }
