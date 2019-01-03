40d5e40ee8c67fbf4c7871eb54132220554e2549
lzr <991192715@qq.com>
2019-01-03 11:17:41 +0800

    错误示范

--- src/main/Java/com/controler/IndexController.java
+++ src/main/Java/com/controler/IndexController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/index")
     public String index() {
+        return 0;
         return "index";
     }
 }
