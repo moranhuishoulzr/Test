d10a27ca83611442e49b4ddb2b4f17d1981fc87f
Zuoran Liang(IT) <Zuoran.Liang@cicc.com.cn>
2019-01-16 15:45:54 +0800

    comit1

--- src/main/Java/com/controler/IndexController.java
+++ src/main/Java/com/controler/IndexController.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/index")
     public String index() {
-        return "comit00000";
+        return "comit1";
     }
 }
